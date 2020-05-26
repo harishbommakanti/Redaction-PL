dae369907bc489cc24966c249ad7638a858f88c5
Harish <harishbommakanti2002@gmail.com>
2020-05-26 16:31:08 -0500

    added a readFile() method in Lexer while brings all the .redact sourcecode down to one string

--- src/Lexer.java
+++ src/Lexer.java
@@ -1,12 +1,31 @@
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Lexer
 {
-    private BufferedReader br = null;
+    //private BufferedReader br = null;
+    private StringBuilder str = new StringBuilder();
 
     public Lexer(String filepath) throws IOException
     {
-        BufferedReader br = new BufferedReader(new FileReader(new File(filepath)));
-        this.br = br;
+        //BufferedReader br = new BufferedReader(new FileReader(new File(filepath)));
+        //this.br = br;
+
+        readfile(filepath);
+    }
+
+    private void readfile(String filepath) throws IOException
+    {
+        byte[] rawdata = Files.readAllBytes(Paths.get(filepath));
+        String rawstring = new String(rawdata);
+
+        //remove all whitespaces and new lines
+        rawstring = rawstring.replace(" ","").trim().replace("\n","");
+
+        str.append(rawstring);
+
+        //test
+        //System.out.println(str.toString());
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 import java.io.*;
 
 //Main class for calling functions from other classes like Lexer and Parser
-//The 'landing page' of the compiler/interpreter
+//The 'landing page' of the compiler/interpreter(byte)
 public class Main
 {
     public static void main(String[] args) throws IOException
     {
         //stage 1 of the compiler is the Lexer, which converts the input program into a sequence of tokens
-        String filepath = "testcode.redact";
+        String filepath = "src/testcode.redact";
         Lexer lex = new Lexer(filepath);
     }
 }
