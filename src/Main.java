029ed0afa6b9882ac84a5ba4eab2be156cbe3af8
Harish <harishbommakanti2002@gmail.com>
2020-05-25 21:21:27 -0500

    added intellij project files to git alongside .gitignores, also added blank Main.java page

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//Main class for calling functions from other classes like Lexer and Parser
+//The 'landing page' of the compiler/interpreter
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
