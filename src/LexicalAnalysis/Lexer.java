75a4763379d7d68934d2c3b6d527b800ce4796e5
harbom <33380236+harbom@users.noreply.github.com>
2020-05-30 18:25:38 -0500

    Lexer (#4)
    * added intellij project files to git alongside .gitignores, also added blank Main.java page
    
    * added an initial Lexer.java file for lexical analysis and added a blank testcode.redact file for very basic starter test code
    
    * Create testcode.redact
    
    * added a readFile() method in Lexer while brings all the .redact sourcecode down to one string
    
    * added the TokenType enum and the Token class to be the objects of the priority queue, which is the goal of the lexer
    
    * interim commit before letting zaid work on the tokenization
    
    * Update Lexer.java
    
    -Changed format of reading token string
    -Fixed queue data type security issue
    
    * Alpha 0.0.1
    
    -Lexer is able to read program file and displace each token with each token identity
    
    -Lexer is currently unable to identity variable names and literals. There is a hardcoded ``5`` to reflect what is in the code
    
    -Token.java and TokenType.java has been removed and replaced with a new Token.java enum class
    
    -testcode.redact has been changed to allow for basic functionality
    
    -Files are all functional to current specification
    
    * Alpha 0.0.1
    
    Update all redactedness from the double revert.
    
    * interim commit for switching over to holding token names in a hashset and using a switch case to add tokens
    
    * interim commit NOT FUNCTIONAL CODE: revampled the Token class with a hashset for reading in token names
    
    * added all of the code for going back to the Token<String name, String content>, need to test now
    
    * printed out all null names but correct content, handing it over to zaid to see if he knows
    
    * Alpha 0.0.2
    
    -bug fixes and ease of readablity of code
    
    * Alpha 0.0.2
    
    -Added lexer recognition of String and char literals
    
    -Added new test case for String and char
    
    -Cleaned up files
    
    * added a 'func ~ FUNCTION' mapping to the text file as the next step in the lexer is to build a symbol table; returned the token list to Main
    
    * added the SymbolTable class and a blank method in Lexer to go through the token list and insert variable/function names to the table
    
    * added a loop to the method and put statements to ignore program begin/end tokens
    
    * NOT FUNCTIONAL CODE: structure for at least dealing with variable initialization in Lexer.insertAllSymbolTables
    
    * deleted unecessary symbol table class, added hashmap as instance variable in Main.java; commented out buildSymbolTable method in Lexer as it should only be done after building AST. Next step is unit testing and then merge to master will be good
    
    * Buggy Code
    
    - Code is NOT functional and will require much tweaking.
    
    -Being sent off to Harish for fixes
    
    * fixed problems with previous commit for breakDownExpression (previously named recurse); commented/cleaned up code
    
    * added more boolean tokens in tokenMapping; removed 'type' from .redact testcodes (redundant); added functionality for lexer to look over 2 character symbols like >= and ==
    
    * Unit Testing
    
    - First unit test case added
    
    * Le New Rule
    
    - Must have space between if, for, while and left parenthesis
    
    * LO Spacing is a requirement
    
    Same as before
    
    * Alpha 0.0.4
    
    -Unit Test 1 is complete!
    - Spacing required
    
    * Alpha 0.0.4
    
    Unit Test 2 being added
    
    * String Literals Not Reading Correctly
    
    - "LO HAW MONKAS"
    
    * STRING_LITERALS must not have spaces in between individual words
    
    title
    
    * Unit Testing!
    
    Unit Testing is almost complete. Test 4 is still in progress.
    
    * Unit Testing Complete!
    
    4 tests render the lexer.java to be function with a few caveats.
    
    * added folder structure to make project more organized; changed filepath for test .redact sourcecodes; added a e.getMessage() to a try-catch block
    
    * also added a documentation folder
    
    Co-authored-by: zmaknojia20 <54285197+zmaknojia20@users.noreply.github.com>
    Co-authored-by: Zaid Maknojia <zmaknojia20@gmail.com>

---
+++ src/LexicalAnalysis/Lexer.java
@@ -0,0 +1,246 @@
+package LexicalAnalysis;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class Lexer
+{
+    //for holding a 1 line representation of the source code
+    private StringBuilder str = new StringBuilder();
+    List<String> list = new ArrayList<String>();
+    Map<String,String> mapping = Token.retMap();
+
+    //the result of Lexer.java
+    private ArrayList<Token> tokenList = new ArrayList<Token>();
+
+    public Lexer(String filepath) throws IOException
+    {
+        readSourceCode(filepath);
+        preProcessString();
+        processList();
+        //printTokenization();
+    }
+
+    //loads source code data into str
+    private void readSourceCode(String filepath) throws IOException
+    {
+        byte[] rawdata = Files.readAllBytes(Paths.get(filepath));
+        String rawstring = new String(rawdata);
+        //remove all whitespaces
+        rawstring = rawstring.trim();
+        str.append(rawstring);    
+    }
+
+    //Converts StringBuilder String into an ArrayList to be tokenized
+    private void preProcessString() throws IOException
+    {
+        String[] s = str.toString().split("\n");
+        Collections.addAll(list,s);
+    }
+
+    //this parses the string formed in readfile and pushes tokens in the queue
+    private void processList()
+    {        
+        //adds all tokens to arraylist
+        for(int i = 0; i < list.size(); i++) 
+        { 
+            String [] arr = list.get(i).split(" "); //split tokens based on " " to seperate things like 'type' and 'int' and identifiers/literals
+            for(int j = 0; j < arr.length; j++) 
+            {
+
+                String curr = arr[j];
+                //special cases to consider before anything else: operators with length 2 like ==, <=, >=
+                //assume the programmer has simple code and only one of these operators appear per statement
+                int symbolIndex = -1;
+                String symbol = "";
+                if (curr.contains("=="))
+                {
+                    symbolIndex = curr.indexOf("==");
+                    symbol = "==";
+                } else if (curr.contains(">="))
+                {
+                    symbolIndex = curr.indexOf(">=");
+                    symbol = ">=";
+                } else if (curr.contains("<="))
+                {
+                    symbolIndex = curr.indexOf("<=");
+                    symbol = "<=";
+                }
+
+                if (symbolIndex != -1) //if one of the 3 special symbols was found
+                {
+                    detectForSymbolsAndBreakDownExp(curr.substring(0,symbolIndex)); //looks at left substring
+                    tokenList.add(new Token(symbol)); //add token for symbol
+                    detectForSymbolsAndBreakDownExp(curr.substring(symbolIndex+2)); //looks at right substring, needs +2 as the length of the character is 2
+
+                    continue; //don't do the other conditional checks, move on to next token
+                }
+
+
+                if(!mapping.containsValue(curr)) //if the token isn't in the mapping: its a identifier or literal
+                {
+                    //need to deal with the case of it being a complex expression: aka `let a = b+c/d type int`
+
+                    detectForSymbolsAndBreakDownExp(curr);
+                }
+                else //case for 1 character symbols
+                {
+                    //it was found in the mapping --> simple search of the hashmap in the token class
+                    tokenList.add(new Token(curr));
+                }
+            }
+        }
+    }
+
+    //need to deal with the case of it being a complex expression: aka `let a = b+c/d type int`
+    private void detectForSymbolsAndBreakDownExp(String curr)
+    {
+        int indexOfFirstSymbol = findIndexOfSymbol(curr); //find index of first symbol
+        curr = curr.replace(" ",""); //remove whitespace within the string to make life easier
+        breakDownExpression(curr, indexOfFirstSymbol); //basically do in order transversal to get tokens in the right order
+        //if no symbol is found, indexOfFirstSymbol will be -1, in which case breakDownExpression will just assign 1 token
+        //and terminate with no further recursion
+    }
+
+    //meant for looking at user created expressions (identifiers/character literals) and adding them to tokenList
+    private void addIdentifierOrLiteral(String str)
+    {
+        if (str.length() == 0) return; //case of "" being passed in as a string
+
+        //case of an integer literal
+        boolean isInt = false;
+        try
+        {
+            int int_literal = Integer.parseInt(str);
+            isInt = true; //if it gets to this point, there was no error, so isInt = true
+        } catch (NumberFormatException e) {}
+        if (isInt)
+        {
+            tokenList.add(new Token("INT_LITERAL", str));
+        }
+
+
+        //case of it being a String
+        else if(str.charAt(0) == '"' && str.charAt(str.length()-1) == '"')
+        {
+            tokenList.add(new Token("STRING_LITERAL",str));
+        }
+
+
+        //case of it being a char                   
+        else if(str.charAt(0) == '\'' && str.charAt(str.length()-1) == '\'')
+        {
+            tokenList.add(new Token("CHAR_LITERAL",str));
+        }
+        else
+        {
+            //if not identified as an int, String, or char, it must be a indentifier
+            //however, logic to be added to verify if identifier name is valid
+            tokenList.add(new Token("IDENTIFIER",str));
+        }
+    }
+
+
+    //keeps splitting the input string into 2 parts and recurse over each part until only strings
+    //without symbols are found. the symbols are added to the token list and the identifiers/literals are
+    //also added in correct order
+    private void breakDownExpression(String str, int index)
+    {
+        if (index == -1)
+        {
+            addIdentifierOrLiteral(str);
+            return;
+        } 
+        else
+        {
+            breakDownExpression(str.substring(0,index),findIndexOfSymbol(str.substring(0,index)));
+            tokenList.add(new Token(str.substring(index,index+1)));
+            breakDownExpression(str.substring(index+1),findIndexOfSymbol(str.substring(index+1)));
+        }
+    }
+
+    //returns the index of the first occurence of a symbol, or -1 if no index was found
+    private int findIndexOfSymbol(String str)
+    {
+        for(int i = 0; i < str.length(); i++)
+        {
+            if(mapping.containsValue(str.substring(i,i+1)))
+                return i;
+        }
+        return -1;
+    }
+
+    //print out index, token, and token identity
+    private void printTokenization()
+    {
+       for(Token t : tokenList)
+        {
+            System.out.println("name: " + t.name + "\tcontent: " + t.content);
+        }
+    }
+
+    //the result of the lexing phase: returns a list of tokens
+    public ArrayList<Token> tokenize()
+    {
+        return tokenList;
+    }
+}
+
+// \/ \/ \/ is part of the Parser, not even needed in the Lexer
+    /*
+    //will go through the token list and inert variable/functions names wherever appropriate
+    public void insertAllSymbolTables(SymbolTable st)
+    {
+        SymbolTable currentSymbolTable = st;
+        for (int i = 0; i < tokenList.size(); i++)
+        {
+            Token currToken = tokenList.get(i);
+            if (currToken.name.equals("PROGRAM_BEGIN") || currToken.name.equals("PROGRAM_END")) //these tokens aren't really important for symbol tables
+                continue;
+
+            //initialization of a variable
+            if (currToken.name.equals("LET"))
+            {
+                Token identifierToken = tokenList.get(i+1);
+                String identifierName = identifierToken.name;
+                //token at i+2 will be an = sign, irrelevant
+
+                //it may be like 'let a = b + c / d' : the value of the array would be more than just a literal etc
+                List<Token> identifierValueTokens = new ArrayList<Token>();
+                int tempIndex = i+3;
+                Token tempToken = tokenList.get(tempIndex);
+                while(!tempToken.name.equals("TYPE")) //add value tokens to the identifierValueTokens list while it doesnt move on to the type signature
+                {
+                    identifierValueTokens.add(tempToken);
+                    tempIndex++;
+                }
+
+                //now, identifierValueTokens has all the tokens to assign to the current identifier
+                //token at tempIndex will now be TYPE, so look at index (tempIndex+1)
+                String type = tokenList.get(tempIndex+1).name;
+
+                //if the type is a char literal or string,
+                String value = "";
+                if (type.equals("CHAR"))
+                    value = calculateCharExpression(identifierValueTokens); //TODO
+                else if (type.equals("STRING"))
+                    value = calculateStringExpression(identifierValueTokens); //TODO
+                else //its an it
+                    value = calculateMathExpression(identifierValueTokens); //TODO
+
+
+
+                //at the very end, after all that parsing is done, add it to the current symbol table
+                Map<String,String> newEntry = new HashMap();
+                newEntry.put("type",type);
+                newEntry.put("value",value);
+                currentSymbolTable.data.put(identifierName,newEntry);
+
+                //update i to go to the next statement
+                i = tempIndex + 2;
+            }
+        }
+    }
+     */
---
+++ src/LexicalAnalysis/UnitTests/unitTesting.java
@@ -0,0 +1,152 @@
+package LexicalAnalysis.UnitTests;
+
+import LexicalAnalysis.Lexer;
+import LexicalAnalysis.Token;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class unitTesting {
+    static String filepath;
+    static Lexer lex;
+    static List<Token> tokenList;
+    static List<Token> unitTestKey;
+    public static void main(String[] args) throws IOException 
+    {
+        System.out.println("Unit Testing: Redaction Programming Language");
+        test1();
+        test2();
+        test3();
+        test4();        
+    }
+
+    private static void test1() throws IOException
+    {
+        filepath = "src/LexicalAnalysis/UnitTests/unitTest1.redact";
+        lex = new Lexer(filepath);
+        tokenList = lex.tokenize();
+        unitTestKey = new ArrayList<>(Arrays.asList(new Token("{begin}"),
+                                                    new Token("let"),new Token("IDENTIFIER","radius"),new Token("="),new Token("INT_LITERAL","6"),
+                                                    new Token("int"), new Token("let"),new Token("IDENTIFIER","area"), new Token("="),
+                                                    new Token("IDENTIFIER","pi"),new Token("*"),new Token("IDENTIFIER","radius"),new Token("*"),
+                                                    new Token("IDENTIFIER","radius"), new Token("double"), new Token("if"),new Token("("),
+                                                    new Token("IDENTIFIER","radius"),new Token("<="),new Token("INT_LITERAL","5"),new Token("and"),
+                                                    new Token("IDENTIFIER","area"),new Token("=="),new Token("INT_LITERAL","213"),new Token(")"),new Token("{"),
+                                                    new Token("let"),new Token("IDENTIFIER","c"),new Token("="),new Token("INT_LITERAL","5"),new Token("+"),
+                                                    new Token("INT_LITERAL","3"),new Token("}"), new Token("func"),new Token("IDENTIFIER","add"),
+                                                    new Token("("),new Token(")"),new Token("{"),new Token("let"),new Token("IDENTIFIER","sum"),new Token("="),
+                                                    new Token("INT_LITERAL","25"),new Token("-"),new Token("INT_LITERAL","7"),new Token("}"),new Token("{end}")));
+        if(compareList(tokenList, unitTestKey))
+            System.out.println("Test 1 : Passed!");
+        else
+            System.out.println("Test 1 : Failed!");
+    }
+
+    private static void test2() throws IOException
+    {
+        filepath = "src/LexicalAnalysis/UnitTests/unitTest2.redact";
+        lex = new Lexer(filepath);
+        tokenList = lex.tokenize();
+        unitTestKey = new ArrayList<>(Arrays.asList(new Token("{begin}"), new Token("loop"), new Token("IDENTIFIER","i"), new Token("from"), 
+                                                    new Token("INT_LITERAL","1"), new Token("to"), new Token("INT_LITERAL","10"),new Token("{"),
+                                                    new Token("IDENTIFIER","print"), new Token("("), new Token("STRING_LITERAL","\"LOHAWMONKAS\""), new Token(")"), 
+                                                    new Token("}"), new Token("if"), new Token("("),new Token("("), new Token("INT_LITERAL","5"), new Token("<"), 
+                                                    new Token("INT_LITERAL","9"), new Token(")"),new Token("and"), new Token("("), new Token("INT_LITERAL","6"), 
+                                                    new Token(">"), new Token("INT_LITERAL","1"), new Token(")"),new Token(")"),new Token("{"),new Token("let"),
+                                                    new Token("IDENTIFIER","b"), new Token("="), new Token("INT_LITERAL","6"), new Token("+"), new Token("INT_LITERAL","4"),
+                                                    new Token("int"), new Token("if"), new Token("("), new Token("("), new Token("IDENTIFIER", "b"), new Token(">="), 
+                                                    new Token("INT_LITERAL","6"), new Token(")"), new Token("or"), new Token("("), new Token("INT_LITERAL","5434"), 
+                                                    new Token("%"), new Token("INT_LITERAL","234"), new Token("=="), new Token("INT_LITERAL","0"),new Token(")"),new Token(")"), 
+                                                    new Token("{"), new Token("IDENTIFIER","print"), new Token("("), new Token("STRING_LITERAL","\"REE\""),
+                                                    new Token(")"), new Token("}"), new Token("}"), new Token("let"), new Token("IDENTIFIER","x"), new Token("="), 
+                                                    new Token("INT_LITERAL","234"), new Token("while"), new Token("("), new Token("IDENTIFIER","x"), new Token(">"), 
+                                                    new Token("INT_LITERAL","0"), new Token(")"),new Token("{"), new Token("IDENTIFIER","print"), new Token("("),
+                                                    new Token("IDENTIFIER","x"), new Token(")"), new Token("IDENTIFIER","x"),new Token("="), new Token("IDENTIFIER","x"),
+                                                     new Token("-"), new Token("INT_LITERAL","1"), new Token("}"), new Token("{end}")));
+        if(compareList(tokenList, unitTestKey))
+            System.out.println("Test 2 : Passed!");
+        else
+            System.out.println("Test 2 : Failed!");
+    }
+
+
+    private static void test3() throws IOException
+    {
+        filepath = "src/LexicalAnalysis/UnitTests/unitTest3.redact";
+        lex = new Lexer(filepath);
+        tokenList = lex.tokenize();
+        unitTestKey = new ArrayList<>(Arrays.asList(new Token("{begin}"), new Token("if"), new Token("!"), new Token("("),
+                                                    new Token("INT_LITERAL","324"), new Token("+"), new Token("INT_LITERAL","214"),
+                                                    new Token("=="), new Token("INT_LITERAL","538"), new Token(")"), new Token("{"), 
+                                                    new Token("let"), new Token("IDENTIFIER","a"),new Token("="), new Token("INT_LITERAL","538"), 
+                                                    new Token("int"), new Token("IDENTIFIER","print"),new Token("("), 
+                                                    new Token("IDENTIFIER","a"), new Token(")"), new Token("}"), new Token("let"),
+                                                    new Token("IDENTIFIER","b"), new Token("="), new Token("CHAR_LITERAL","'b'"), 
+                                                    new Token("char"), new Token("let"), new Token("IDENTIFIER","c"), new Token("="),
+                                                    new Token("false"), new Token("bool"), new Token("const"), new Token("IDENTIFIER","d"),
+                                                    new Token("="), new Token("STRING_LITERAL","\"Hi\""),new Token("String"), new Token("let"),
+                                                    new Token("IDENTIFIER","e"), new Token("="), new Token("IDENTIFIER","d"), new Token("+"), 
+                                                    new Token("STRING_LITERAL","\"ThisLanguageIsVeryPoggers\""),
+                                                    new Token("String"), new Token("{end}")));
+        if(compareList(tokenList, unitTestKey))
+            System.out.println("Test 3 : Passed!");
+        else
+            System.out.println("Test 3 : Failed!");
+
+    }
+
+    private static void test4() throws IOException
+    {
+        filepath = "src/LexicalAnalysis/UnitTests/unitTest4.redact";
+        lex = new Lexer(filepath);
+        tokenList = lex.tokenize();
+        unitTestKey = new ArrayList<>(Arrays.asList(new Token("{begin}"), new Token("if"), new Token("("),new Token("STRING_LITERAL","\"LO\""),
+                                                    new Token("=="), new Token("STRING_LITERAL","\"LO\""), new Token(")"), new Token("{"), new Token("let"),
+                                                    new Token("IDENTIFIER","a"), new Token("="), new Token("CHAR_LITERAL","'a'"), new Token("char"),
+                                                    new Token("}"), new Token("elif"), new Token("{"), new Token("let"), new Token("IDENTIFIER","a"), 
+                                                    new Token("="), new Token("INT_LITERAL","65"), new Token("int"), new Token("}"), new Token("let"),
+                                                    new Token("IDENTIFIER","x"), new Token("="), new Token("("), new Token("("), new Token("INT_LITERAL","4"), new Token("/"),
+                                                    new Token("INT_LITERAL","2"), new Token(")"),new Token("+"),new Token("("),new Token("INT_LITERAL","213"),
+                                                    new Token("*"),new Token("INT_LITERAL","234"), new Token("%"), new Token("INT_LITERAL","12"), new Token(")"),
+                                                    new Token("-"), new Token("INT_LITERAL","123"), new Token(")"),new Token("int"), new Token("loop"), new Token("IDENTIFIER","i"),
+                                                    new Token("from"), new Token("INT_LITERAL","0"), new Token("to"), new Token("IDENTIFIER","x"), new Token("{"), new Token("loop"),
+                                                    new Token("IDENTIFIER","j"), new Token("from"), new Token("INT_LITERAL","0"), new Token("to"), new Token("IDENTIFIER","x"), 
+                                                    new Token("{"), new Token("let"), new Token("IDENTIFIER","LO"), new Token("="), new Token("IDENTIFIER","i"),
+                                                    new Token("*"),new Token("IDENTIFIER","j"), new Token("int"), new Token("IDENTIFIER","print"), new Token("("),
+                                                    new Token("IDENTIFIER","LO"),new Token(")"),new Token("}"),new Token("}"),new Token("{end}")));
+        if(compareList(tokenList, unitTestKey))
+            System.out.println("Test 4 : Passed!");
+        else
+            System.out.println("Test 4 : Failed!");
+    }
+
+
+
+
+    private static boolean compareList(List<Token>list1, List<Token>list2)
+    {
+        for(int i = 0; i < list1.size(); i++)
+        {
+            if (!(Token.comparisonForString(list1.get(i)).equals(Token.comparisonForString(list2.get(i)))))
+                return false;
+        }
+            return true;
+    }
+    /*
+    private static boolean compareToken(Token t1, Token t2)
+    {
+            if (!(Token.comparisonForString(t1).equals(Token.comparisonForString(t2))))
+                return false;
+            return true;
+    }
+    
+    private static void printList(List<Token>list1, List<Token>list2) 
+    {
+        for(int i = 0; i < list1.size(); i++)
+        {
+            System.out.println(Token.comparisonForString(list1.get(i)) + "                " +Token.comparisonForString(list2.get(i))+ "             "+compareToken(list1.get(i),list2.get(i)));
+        }
+    }*/
+}
